5126cca488d37ee8e53bd5c82e4302c5bbab7b1f
bryttanie <bhouse@pdx.edu>
2020-10-06 19:49:16 -0700

    starting settings page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import SettingsPage from 'components/SettingsPage.js';
 import './App.css';
-import './styles/theme.sass'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main/App.sass';
+import './styles/main/theme.sass';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="test">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SettingsPage />
     </div>
   );
 }
