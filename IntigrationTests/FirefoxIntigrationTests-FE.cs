7f305633f952119c942b325c85f23222ea9c6409
Brandon <bld@pdx.edu>
2020-10-09 23:21:38 -0700

    initial commit

---
+++ IntigrationTests/FirefoxIntigrationTests-FE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace PSUCaptstoneTestingProject.Front_end
+{
+    class FirefoxIntigrationTestsF
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
