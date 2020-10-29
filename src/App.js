d01e91eb0d7fdbe728724d64904e0589367c4549
nguyethy002 <minhthy0120032003@gmail.com>
2020-10-28 20:47:23 -0700

    Cleaning sprint1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Login } from "./pages";
 import './App.css';
-import './styles/main/theme.sass';
+
 import Select_item  from './Page/select_item';
 
 function App() {
   return (
     <div>
-      <Select_item></Select_item>
-        <Login/>
+      {/* <Select_item></Select_item> */}
+        {/* <Login/> */}
 
     </div>
   );
