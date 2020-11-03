2996142b9d1f97563590c8511f2f8575e0d4d0fc
nguyethy002 <minhthy0120032003@gmail.com>
2020-11-02 17:29:18 -0800

    Working on sync error

---
+++ src/components/SyncError.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import "../styles/components/SyncError.style.sass";
+import ErrorIcon from "@atlaskit/icon/glyph/error";
+import Banner from "@atlaskit/banner";
+
+const SyncError = () => {
+  axios
+    .get(`http://127.0.0.1:5000/get_logs`)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch(() => {});
+
+  return (
+    <div className="syncerror-container">
+      <Banner
+        appearance="error"
+        icon={<ErrorIcon label="Error icon" secondaryColor="inherit" />}
+        isOpen
+      >
+        Error message goes here
+      </Banner>
+    </div>
+  );
+};
+
+export default SyncError;
