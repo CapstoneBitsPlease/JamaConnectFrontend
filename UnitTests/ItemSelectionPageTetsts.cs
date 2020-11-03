b295c5e923b88108e36e488c67fbdea2067101a6
Brandon <bld@pdx.edu>
2020-11-03 11:27:35 -0800

    addig sync and item selection tests

---
+++ UnitTests/ItemSelectionPageTetsts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PSUCapstoneTestingProject.Front_end.UnitTests
+{
+    class ItemSelectionPageTetsts
+    {
+        IWebDriver driver;
+        IWebElement username_input;
+        IWebElement password_input;
+        IWebElement organization_input;
+        IWebElement login_button;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver("C:/Users/Brandon Danielski/Documents");
+            driver.Url = "http://localhost:3000/";
+            username_input = driver.FindElement(By.Id("username"));
+            password_input = driver.FindElement(By.Id("password"));
+            organization_input = driver.FindElement(By.Id("organization"));
+            login();
+        }
+
+        public void login()
+        {
+            driver.Navigate();
+            username_input.Clear();
+            password_input.Clear();
+            organization_input.Clear();
+            username_input.SendKeys("capstone_tester");
+            password_input.SendKeys("capstoneBITZpls!0");
+            organization_input.SendKeys("capstone2020");
+            driver.FindElement(By.Id("signin")).Click();
+            System.Threading.Thread.Sleep(1000);//Need to wait a second to get the swal2-title to get created.
+        }
+
+        [Test]
+        public void item_select_happy_path()
+        {
+            Assert.Pass();
+        }
+    }
+
+
+}
---
+++ UnitTests/SyncPageTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace PSUCapstoneTestingProject.Front_end.UnitTests
+{
+    class SyncPageTests
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver("C:/Users/Brandon Danielski/Documents");
+            driver.Url = "http://localhost:3000/syncSettings";
+            driver.Navigate();
+        }
+
+        [Test]
+        public void sync_happy_path()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void sync_happy_path_seconds()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("seconds");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("5");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void sync_happy_path_minutes()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("minutes");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("5");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void sync_happy_path_hours()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("hours");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("5");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void sync_bad_input_seconds()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("seconds");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("bad");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void sync_bad_input_minutes()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("minutes");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("bad");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void sync_bad_input_hours()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("hours");
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys("bad");
+            driver.FindElement(By.Id("apply_button")).Click();
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+            driver.FindElement(By.Id("select_input_text_field")).SendKeys(Keys.Backspace);
+        }
+
+        [Test]
+        public void no_input_seconds()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("seconds");
+            driver.FindElement(By.Id("apply_button")).Click();
+        }
+
+        [Test]
+        public void no_input_minutes()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("minutes");
+            driver.FindElement(By.Id("apply_button")).Click();
+        }
+
+        [Test]
+        public void no_input_hours()
+        {
+            new SelectElement(driver.FindElement(By.Id("dropdown_list_selection"))).SelectByText("hours");
+            driver.FindElement(By.Id("apply_button")).Click();
+        }
+    }
+}
