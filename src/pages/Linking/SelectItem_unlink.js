1af4361aed15b266af37b2a91bb8c0a9cd6cbb29
yi-xuan-97 <56617127+yi-xuan-97@users.noreply.github.com>
2020-11-18 19:17:39 -0800

    fixed some issue

--- src/pages/Linking/SelectItem_unlink.js
+++ src/pages/Linking/SelectItem_unlink.js
@@ -31,6 +31,9 @@ const SelectItemunlink = () => {
 				}
 			})
 			.then(res => {
+				if(res.data.length == 0){
+					makeToast("error", "There is no Jama item")
+				}
 				console.log(res);
 				setlist(res.data);
 			})
