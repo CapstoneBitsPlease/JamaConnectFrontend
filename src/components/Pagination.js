9030013b29332e78560f995e2ecded9c0e9ada38
nguyethy002 <minhthy0120032003@gmail.com>
2020-11-24 14:43:36 -0800

    Finished the date filter and pagination for error logs

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({postsPerPage, totalPosts, paginate}) => {
-  const pageNumbers =[];
-  for(let i =1; i<= Math.ceil(totalPosts/postsPerPage); i++){
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
-  console.log("post per page",postsPerPage);
-  console.log("total posts",totalPosts);
+  console.log("post per page", postsPerPage);
+  console.log("total posts", totalPosts);
   return (
     <div>
-      {pageNumbers.map(number=>(
-        <p key ={number}>
-          <button onClick={()=>paginate(number)} >
-            {number}
-          </button>
-        </p>
+      {pageNumbers.map((number) => (
+        <button key={number} onClick={() => paginate(number)}>
+          {number}
+        </button>
       ))}
     </div>
-  )
-}
+  );
+};
 export default Pagination;
